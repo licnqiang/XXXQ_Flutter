7b81eda9efe12ad73ed319fe198ba2b82a4960f3
luosen <lzh92618>
2019-11-20 16:34:41 +0800

    登录页面绘制完成

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:xxxq_flutter/main.dart';
 
 /**
  * 登录页
@@ -14,6 +15,11 @@ class LoginPage extends StatefulWidget{
 
 class LoginPageState extends State<LoginPage>{
 
+  //手机号的控制器
+  TextEditingController nameController = TextEditingController();
+
+  //密码的控制器
+  TextEditingController pwdController = TextEditingController();
 
   String _name,_password;
   bool _isObscure = true;
@@ -22,26 +28,35 @@ class LoginPageState extends State<LoginPage>{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-          key: _formKey,
-          child: ListView(
-            padding: EdgeInsets.symmetric(horizontal: 22.0),
-            children: <Widget>[
-              SizedBox(height: kToolbarHeight,),
-              buildTitle(),
-              SizedBox(height: 70.0,),
-              buildNameTextField(),
-              SizedBox(height: 70.0,),
-              buildPasswordTextField(context),
-              SizedBox(height: 60.0),
-              buildLoginButton(context),
-              SizedBox(height: 30.0),
+    return
+      MaterialApp(home:
+      Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/xh.png"),
+                  fit: BoxFit.cover,
+          ),
+          ),
+          child: Form(
+              key: _formKey,
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 22.0),
+                children: <Widget>[
+                  SizedBox(height: kToolbarHeight,),
+                  buildTitle(),
+                  SizedBox(height: 80.0),
+                  buildNameTextField(),
+                  SizedBox(height: 50.0),
+                  buildPasswordTextField(context),
+                  SizedBox(height: 60.0),
+                  buildLoginButton(context),
 
-            ],
-      )),
-      
-    );
+                ],
+              )),
+        ),
+      ) ,);
+
   }
 
 
@@ -53,7 +68,7 @@ class LoginPageState extends State<LoginPage>{
         child: RaisedButton(
           child: Text(
             '登录',
-              style: TextStyle(color: Colors.white,fontSize: 20.0),
+              style: TextStyle(color: Colors.white,fontSize: 18.0),
           ),
           color: Colors.blue,
           onPressed: () {
@@ -61,10 +76,16 @@ class LoginPageState extends State<LoginPage>{
               ///只有输入的内容符合要求通过才会到达此处
               _formKey.currentState.save();
               //TODO 执行登录方法
-              print('name:$_name , password:$_password');
+              print({'name':nameController.text,'pwd':pwdController.text});
+
+              Navigator.push(context, new MaterialPageRoute(builder: (context)=>new MyApp()));
+
             }
           },
-          shape: StadiumBorder(side: BorderSide()),
+//          shape: StadiumBorder(side: BorderSide(color: Colors.blue)),
+        shape: const RoundedRectangleBorder(
+          side: BorderSide.none,borderRadius: BorderRadius.all(Radius.circular(10))
+        ),
         ),
       ),
     );
@@ -72,8 +93,47 @@ class LoginPageState extends State<LoginPage>{
 
 
 
-  TextFormField buildPasswordTextField(BuildContext context){
-    return TextFormField(
+  ConstrainedBox buildPasswordTextField(BuildContext context){
+
+    return ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: 45,
+          maxWidth: 200,
+        ),
+
+      child: new TextField(
+        obscureText: _isObscure,
+        decoration: InputDecoration(
+
+          contentPadding: const EdgeInsets.symmetric(vertical: 4.0),
+          hintText: '请输入密码',
+          hintStyle: TextStyle(color: Color.fromARGB(255, 91, 91, 91),fontSize: 14.0),
+
+          prefixIcon: Icon(Icons.lock),
+          suffixIcon: IconButton(icon: Icon(Icons.remove_red_eye,color: _eyeColor,),
+          onPressed: () {
+            setState(() {
+            _isObscure = !_isObscure;
+             _eyeColor = _isObscure
+            ? Colors.grey
+           : Theme.of(context).iconTheme.color;
+    });
+           },
+        ),
+
+        border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide.none),
+        filled: true,
+        fillColor:Color.fromARGB(77, 255, 255, 255),//内部颜色
+      ),
+
+        controller: pwdController,
+      ),
+    );
+
+
+/*    return TextFormField(
       onSaved: (String value)=> _password=value,
       obscureText: _isObscure,
       validator: (String value){
@@ -83,7 +143,8 @@ class LoginPageState extends State<LoginPage>{
       },
 
       decoration: InputDecoration(
-        labelText: '请输入密码',
+        hintText: '请输入密码',
+        hintStyle: TextStyle(color: Color.fromARGB(255, 91, 91, 91),fontSize: 14.0),
         suffixIcon: IconButton(icon: Icon(Icons.remove_red_eye,color: _eyeColor,),
           onPressed: (){
             setState(() {
@@ -93,32 +154,60 @@ class LoginPageState extends State<LoginPage>{
                   : Theme.of(context).iconTheme.color;
             });
           },
-
         )
         ),
 
-    );
+    );*/
   }
 
-  TextFormField buildNameTextField(){
-    return TextFormField(
-      decoration: InputDecoration(
+  ConstrainedBox buildNameTextField(){
 
-        labelText:'请输入用户名'),
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+          maxHeight: 45,
+          maxWidth: 200
+      ),
+      child: new TextField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.symmetric(vertical: 4.0),
+          hintText: '请输入用户名',
+            hintStyle: TextStyle(color: Color.fromARGB(255, 91, 91, 91),fontSize: 14.0),
+          prefixIcon: Icon(Icons.perm_identity),
+          // contentPadding: EdgeInsets.all(10),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide.none),
+          filled: true,
+          fillColor:Color.fromARGB(77, 255, 255, 255),//内部颜色
+
+        ),
+
+        controller: nameController,
+      ),
+
+    );
+
+/*    return TextFormField(
+      decoration: InputDecoration(
+      hintText:'请输入用户名',
+       hintStyle: TextStyle(color: Color.fromARGB(255, 91, 91, 91),fontSize: 14.0),
+
+      ),
       validator: (String value){
         if(value.isEmpty){
           return '请输入正确的用户名';
         }
       },
       onSaved: (String value) =>_name=value,
-    );
+    );*/
   }
 
 
   Padding buildTitle(){
     return Padding(
       padding: EdgeInsets.all(8.0),
-      child: Text('您好，请登录',style: TextStyle(fontSize: 30.0),
+      child: Text('您好，请登录',style: TextStyle(fontSize: 28.0,color: Colors.white,fontWeight: FontWeight.w600),
+
       ),
     );
   }
